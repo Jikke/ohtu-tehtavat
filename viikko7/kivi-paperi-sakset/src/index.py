c9686b7f4cf70f947d3189c02eaa0d49cee77395
Jirko Leivo <jirko.leivo@gmail.com>
2021-12-14 17:07:25 +0200

    Viikon 7 tehtävät.

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -2,8 +2,7 @@ from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
 from kps_tekoaly import KPSTekoaly
 from kps_parempi_tekoaly import KPSParempiTekoaly
 
-
-def main():
+def main(kaksinpeli, yksinpeli, haastava_yksinpeli):
     while True:
         print("Valitse pelataanko"
               "\n (a) Ihmistä vastaan"
@@ -19,25 +18,28 @@ def main():
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            kaksinpeli = KPSPelaajaVsPelaaja()
+            
             kaksinpeli.pelaa()
         elif vastaus.endswith("b"):
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            yksinpeli = KPSTekoaly()
+            
             yksinpeli.pelaa()
         elif vastaus.endswith("c"):
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            haastava_yksinpeli = KPSParempiTekoaly()
+            
             haastava_yksinpeli.pelaa()
         else:
             break
 
 
 if __name__ == "__main__":
-    main()
+    kaksinpeli = KPSPelaajaVsPelaaja()
+    yksinpeli = KPSTekoaly()
+    haastava_yksinpeli = KPSParempiTekoaly()
+    main(kaksinpeli, yksinpeli, haastava_yksinpeli)
